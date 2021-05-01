8135a1831e67bc815e493e07818cd67f527f7889
Johny W. Alves <johnywalves@gmail.com>
2021-05-01 14:15:01 -0300

    :wrench: Configurar conexão para postgres

--- config/database.js
+++ config/database.js
@@ -4,8 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'react-shop'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
       options: {
         useNullAsDefault: true,
